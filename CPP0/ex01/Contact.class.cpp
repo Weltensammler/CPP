384ab2aea8f12c23b14181b1c2176dd13bbb13c7
Weltensammler <79635532+Weltensammler@users.noreply.github.com>
2022-08-25 21:40:28 +0200

    set what

--- CPP0/ex01/Contact.class.cpp
+++ CPP0/ex01/Contact.class.cpp
@@ -6,7 +6,7 @@
 /*   By: bschende <bschende@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 16:30:34 by bschende          #+#    #+#             */
-/*   Updated: 2022/08/24 17:16:29 by bschende         ###   ########.fr       */
+/*   Updated: 2022/08/25 19:53:52 by bschende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,46 @@ Contact::Contact(void)
 
 void Contact::details(void)
 {
-	std::cout << "Enter first Name:" << std::endl;
-	getline(std::cin, fname);
-	std::cout << "Enter last Name:" << std::endl;
-	getline(std::cin, lname);
-	std::cout << "Enter nickname:" << std::endl;
-	getline(std::cin, nname);
-	std::cout << "Enter phone number:" << std::endl;
-	getline(std::cin, phone);
-	std::cout << "Enter darkest secret:" << std::endl;
-	getline(std::cin, dsecret);
+	while (1)
+	{
+		std::cout << "Enter first Name:" << std::endl;
+		getline(std::cin, fname);
+		if (fname.length() > 0)
+			break;
+		std::cout << "Please fill out all the details (no empty fields)" << std::endl;
+	}
+	while (1)
+	{
+		std::cout << "Enter last Name:" << std::endl;
+		getline(std::cin, lname);
+		if (lname.length() > 0)
+			break;
+		std::cout << "Please fill out all the details (no empty fields)" << std::endl;
+	}
+	while (1)
+	{
+		std::cout << "Enter nickname:" << std::endl;
+		getline(std::cin, nname);
+		if (nname.length() > 0)
+			break;
+		std::cout << "Please fill out all the details (no empty fields)" << std::endl;
+	}
+	while (1)
+	{
+		std::cout << "Enter phone number:" << std::endl;
+		getline(std::cin, phone);
+		if (phone.length() > 0)
+			break;
+		std::cout << "Please fill out all the details (no empty fields)" << std::endl;
+	}
+	while (1)
+	{
+		std::cout << "Enter darkest secret:" << std::endl;
+		getline(std::cin, dsecret);
+		if (dsecret.length() > 0)
+			break;
+		std::cout << "Please fill out all the details (no empty fields)" << std::endl;
+	}
 }
 
 // void Contact::setName(std::string name)
--- CPP0/ex01/phonebook.cpp
+++ CPP0/ex01/phonebook.cpp
@@ -6,13 +6,38 @@
 /*   By: bschende <bschende@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 10:35:52 by bschende          #+#    #+#             */
-/*   Updated: 2022/08/24 17:33:31 by bschende         ###   ########.fr       */
+/*   Updated: 2022/08/25 20:44:14 by bschende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "PhoneBook.class.hpp"
 #include "Contact.class.hpp"
+
+int	checkinput(std::string str, int index, int count)
+{
+	int	i;
+
+	if (str.length() > 1 || str.length() < 1)
+		return (1);
+	if (!isdigit(str[0]))
+		return (1);
+	if (count == 8)
+	{
+		if (str[0] > '8' || str[0] < '1')
+			return (1);
+		else
+			return (0);
+	}
+	else
+	{
+		i = stoi(str);
+		if (i > index || str[0] < '1')
+			return (1);
+		else
+			return (0);
+	}
+}
 
 std::string	formatcontact(std::string str)
 {
@@ -88,8 +113,10 @@ int	main(void)
 	int			i = 0;
 	int			j = 0;
 	int			x = 0;
+	int			count;
 
 	input = "ONE";
+	count = 0;
 	while (input.compare("EXIT"))
 	{
 		std::cout << "What would you like to do?\nAdd new Contact.......enter \"ADD\".\nSearch for a Contact..enter \"SEARCH\".\nExit the Phonebook....enter \"EXIT\"" << std::endl;
@@ -99,12 +126,15 @@ int	main(void)
 			details.details();
 			book[i] = savecontact(details);
 			i++;
-			// else
-			// 	std::cout << "Please fill out all the details!" << std::endl;
+			if (i > 7)
+			{
+				i = 0;
+				count = 8;
+			}
 		}
 		if (!input.compare("SEARCH"))
 		{
-			while (j < i)
+			while (j < i || j < count)
 			{
 				contactsearch(j, book[j]);
 				j++;
@@ -113,13 +143,13 @@ int	main(void)
 			{
 				std::cout << "Please choose a Contact to be displayed (Enter indexnumber):" << std::endl;
 				getline(std::cin, c);
-				x = stoi(c);
+				if (!checkinput(c, i, count))
+					x = stoi(c);
 			}
 			outputdetails(book[x - 1]);
+			x = 0;
 			j = 0;
 		}
-		if (i > 7)
-			i = 0;
 	}
 	return (0);
 }
